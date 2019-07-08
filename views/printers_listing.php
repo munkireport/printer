a5bbb6642088f97f2eac18e66698a612fc6fb868
Arjen van Bochoven <nejra@me.com>
2019-07-08 17:31:57 +0200

    Remove model init from view

--- views/printers_listing.php
+++ views/printers_listing.php
@@ -1,12 +1,5 @@
 <?php $this->view('partials/head'); ?>
 
-<?php
-//Initialize models needed for the table
-new Machine_model;
-new Reportdata_model;
-new Printer_model;
-?>
-
 <div class="container">
 
   <div class="row">
